1920665a5a2919e5f25e36a6dae3982323cca7af
Shawn Fowler <70285047+safowler77@users.noreply.github.com>
2023-03-23 11:33:22 -0400

    Add files via upload

---
+++ TestClassAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProductsApp.Tests
+{
+    internal class TestClassAttribute : Attribute
+    {
+    }
+}
---
+++ TestMethodAttribute.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace ProductsApp.Tests
+{
+    internal class TestMethodAttribute : Attribute
+    {
+    }
+}
